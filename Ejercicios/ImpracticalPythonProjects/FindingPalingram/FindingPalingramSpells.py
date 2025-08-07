f5adfdb214ab8ec3ae19765a12125bdec5b18fe5
davidtena <davidtena887@hotmail.com>
2025-08-07 08:57:53 +0200

    Casi

--- Ejercicios/ImpracticalPythonProjects/FindingPalingram/FindingPalingramSpells.py
+++ Ejercicios/ImpracticalPythonProjects/FindingPalingram/FindingPalingramSpells.py
@@ -1,1 +1,7 @@
-import 
+dic1 = "C:\\Users\\david\\Nextcloud\\David\\learning-python\\Ejercicios\\ImpracticalPythonProjects\\FindingPalingram\\12dicts-6.0.2\\International\\3of6all.txt"
+f = open(dic1, "r")
+
+for w in f:
+    w2 = w[::-1]
+    if w == w2:
+        print(w)
